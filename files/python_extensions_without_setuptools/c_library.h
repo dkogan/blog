1b8a39f39f22dec4853808459214db62ed81d301
Dima Kogan <dima@secretsauce.net>
2019-03-22 17:45:50 -0700

    updated python-without-setuptools post

--- files/python_extensions_without_setuptools/c_library.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-// This is a C library. We can call it from C or use the python bindings
-
-void f(void);
--- files/python_extensions_without_setuptools/test.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python2
-
-import project.pymodule
-import project.c_library
-
-project.c_library.f()
-project.pymodule .g()
